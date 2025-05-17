32843c127dcdee4d84942ebae5b2c5ee9b7ac9fe
jportuguez <jossetpr@gmail.com>
2025-05-17 15:04:30 -0500

    primer commit

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()],
+    resolve: {
+      alias: {
+        "@": new URL("./src", import.meta.url).pathname,
+      },
+    },
+  },
+});
