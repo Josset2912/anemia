78cfd3546fd76ad0298a6f5b1d90a56501c588da
jportuguez <jossetpr@gmail.com>
2025-05-22 21:48:06 -0500

    quinto commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import netlify from "@astrojs/netlify"; // <--- 👈 Importar el adaptador
 
 export default defineConfig({
+  output: "server", // <--- 👈 Importante para Netlify
+  adapter: netlify(), // <--- 👈 Aquí se conecta el adaptador
   vite: {
     plugins: [tailwindcss()],
     resolve: {
